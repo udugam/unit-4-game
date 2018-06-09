06773c8089f7c4574910d4e5126ae773e212aae5
udugam <ryan.udugam@gmail.com>
2018-06-09 12:59:42 -0400

    completed game logic for health and attack points

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,33 +14,67 @@ $(document).ready(function () {
         var charHealth = $("<div>");
         charHealth.addClass("health");
         charHealth.text(element.health);
-        charHealth.append
+        
+        //Add data attributes to each character
+        charDiv.data("health",element.health);
+        charDiv.data("attack", element.attack);
+        charDiv.data("counterAttack", element.counterAttack);
+
         charDiv.append(charHealth);
 
         $(".characterSelection").append(charDiv);
     });
 
+    
     //on click of a character, the character is moved to the selectedCharacter 
     //div while the remaining characters are moved to the enemySelection div
     $(".character").on("click", function () {
         var char = $(this);
         char.detach();
+        char.removeClass("character");
+        char.addClass("player");
         $(".selectedCharacter").append(char);
         $(".character").each(function(index,element) {
             if (char.is($(element))===false) {
                 $(element).detach();
-                $(element).removeClass("selectedCharacter")
+                $(element).removeClass("character")
                 $(element).addClass("enemy")
                 $(".enemySelection").append($(element));
             } 
         })
+        
+        
     })
-
+    
     //on enemy click, the character is moved into the defender div
-    $(".enemy").on("click", function() {
+    $("body").on("click", ".enemy", function() {
         $(this).detach();
-        $(this).addClass("inBattle");
+        $(this).removeClass("enemy player")
+        $(this).addClass("oponent");
         $(".defender").append($(this));
     })
+
+    //on click of Attack button:
+    //1. subtract the player's attack from the oponent's health
+    //2. subtract the oponent's counterAttac from the oponent's health
+    //3. execute checkWin() function 
+    $("button").on("click", function() {
+        var attack = $(".player").data("attack");
+        var counterAttack = $(".oponent").data("counterAttack");
+        var playerHealth = $(".player").data("health");
+        var oponentHealth =  $(".oponent").data("health");
+        
+        //1.
+        oponentHealth -= attack;
+        playerHealth -= counterAttack;
+        
+        $(".player").data("health", playerHealth);
+        $(".player .health").text(playerHealth);
+        
+        
+        $(".oponent").data("health", oponentHealth);
+        $(".oponent .health").text(oponentHealth);
+    })
+    
 })
 
