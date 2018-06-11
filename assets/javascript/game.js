7800083d5111582ac7938b78024981c3ae23faf2
udugam <ryan.udugam@gmail.com>
2018-06-11 00:13:56 -0400

    completed basic game logic including reset. Added character pictures

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,20 @@
 $(document).ready(function () {
     var characters = [
-        { name: "Obi-Wan Kenobi", picture: "../images/obi-wan.jpg", health: 120, attack: 10, counterAttack: 20 },
-        { name: "Luke Skywalker", picture: "../images/skywalker.jpg", health: 100, attack: 6, counterAttack: 15 },
-        { name: "Darth Sidious", picture: "../images/sidious.jpg", health: 150, attack: 12, counterAttack: 30 },
+        { name: "Obi-Wan Kenobi", picture: "./assets/images/obi-wan.jpg", health: 120, attack: 10, counterAttack: 20 },
+        { name: "Luke Skywalker", picture: "./assets/images/skywalker.jpg", health: 100, attack: 6, counterAttack: 15 },
+        { name: "Darth Vader", picture: "./assets/images/vader.jpg", health: 150, attack: 12, counterAttack: 30 },
+        { name: "Storm Trooper", picture: "./assets/images/storm-trooper.jpg", health: 100, attack: 5, counterAttack: 10 }
     ]
 
+    //Global attack variable to keep track of increasing attack damage for each game
     var attack = 0;
-
-    //create divs for each character in the array and add them to the page
-    characters.forEach(function (element) {
-        var charDiv = $("<div class='character'>");
-
-        var charName = $("<div class='name'>");
-        charName.text(element.name);
-
-        var charHealth = $("<div class='health'>");
-        charHealth.text(element.health);
-        
-        //Add data attributes to each character
-        charDiv.data("health", element.health);
-        charDiv.data("attack", element.attack);
-        charDiv.data("counterAttack", element.counterAttack);
-
-        charDiv.append(charName,charHealth);
-
-        $(".characterSelection").append(charDiv);
-    });
-
+    
+    //launches starGame function that creates and displays divs for each character
+    startGame();
     
     //on click of a character, the character is moved to the selectedCharacter 
     //div while the remaining characters are moved to the enemySelection div
-    $(".character").on("click", function () {
+    $("body").on("click", ".character", function () {
         var char = $(this);
         char.detach();
         char.removeClass("character");
@@ -57,7 +41,7 @@ $(document).ready(function () {
     })
 
     
-    $("button").on("click", function() {
+    $("button#attackBtn").on("click", function() {
         attack += $(".player").data("attack");
         var counterAttack = $(".oponent").data("counterAttack");
         var playerHealth = $(".player").data("health");
@@ -86,10 +70,59 @@ $(document).ready(function () {
         checkWin();
     })
 
-    //the checkWin function checks to see if the oponents health is 0 or less and removes the element from the DOM
+    //this function is envoked when the newGame Button is clicked. The function will clear all divs and call the startGame() function
+    $("body").on("click", "button#newGameBtn",function() {
+        $(this).remove();
+        $(".matchResult").text("");
+        $(".player").remove();
+        $(".enemy").remove();
+        $(".oponent").remove();
+        startGame();
+    })
+
+    //this function initializes all variables and divs before the game starts
+    
+
+    function startGame() {
+        //create divs for each character in the array and add them to the page
+        characters.forEach(function (element) {
+            var charDiv = $("<div class='character'>");
+    
+            var charPic = $("<img class='picture'>");
+            charPic.attr("src", element.picture);
+            charPic.attr("alt", element.name);
+    
+            var charName = $("<div class='name'>");
+            charName.text(element.name);
+    
+            var charHealth = $("<div class='health'>");
+            charHealth.text(element.health);
+            
+            //Add data attributes to each character
+            charDiv.data("health", element.health);
+            charDiv.data("attack", element.attack);
+            charDiv.data("counterAttack", element.counterAttack);
+    
+            charDiv.append(charName,charPic,charHealth);
+    
+            $(".characterSelection").append(charDiv);
+        });
+    }
+
+    //the checkWin function checks to see if the oponent or player's health is 0 or less and removes the element from the DOM
     function checkWin() {
-        if($(".oponent").data("health") <= 0){
-            $(".oponent").remove();        
+        if ($(".player").data("health") <= 0) {
+            $(".matchResult").text("You've been defeated...GAME OVER!");
+            $(".attackMessage").text(""); 
+            $(".counterAttackMessage").text("");
+            $(".messages").append("<button id='newGameBtn'>New Game</button>")
+        } else if($(".oponent").data("health") <= 0){
+            $(".matchResult").text("You have defeated " + $(".oponent .name").text() + ", choose another enemy.");
+            $(".attackMessage").text(""); 
+            $(".counterAttackMessage").text("");     
+            $(".oponent").remove();
+        } else {
+            $(".matchResult").text("");
         }
     }
     
